6bcd16cd0361aeb5b34c8b58ab709c684c9e1af6
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2025-01-13 14:55:14 +0530

    Passing Children

--- 18 Fragment/learning fragment/src/App.jsx
+++ 18 Fragment/learning fragment/src/App.jsx
@@ -2,15 +2,22 @@ import Fooditems from "./Components/Fooditems";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ErrorMessage from "./Components/ErrorMessage";
+import Container from "./Components/Container";
 function App() {
   // let fooditems=[];
   let fooditems = ["Dal", "Rice", "Green veggy", "Milk", "Roti"];
 
   return (
     <>
+    <Container>
       <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage food={fooditems}></ErrorMessage>
       <Fooditems food={fooditems}></Fooditems>
+    </Container>
+
+    <Container>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores molestias deleniti itaque qui. Sed sapiente ipsam explicabo repudiandae soluta nemo non fuga vero. Explicabo illum neque labore unde! Voluptates, voluptatibus?</p>
+    </Container>
     </>
   );
 }
